97ca181f5312f1c7a12a44483b80e3fdf301a923
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-03-10 12:51:36 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (73.19%) - LeetSync

---
+++ 344-reverse-string/reverse-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n = s.length;
+        int start = 0, end = n - 1;
+        while(start < end){
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
