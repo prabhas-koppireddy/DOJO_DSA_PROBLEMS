82b4dc7423a0a303dcf9e88ff480827a98f78313
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-03-16 20:07:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (95.00%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int ans = 0;
+        for(int i = 0;i < stones.length();i ++){
+            char ch = stones.charAt(i);
+            boolean found = false;
+            for(int j = 0;j < jewels.length();j ++){
+                char dia = jewels.charAt(j);
+                if(ch == dia){
+                    found = true;
+                    break;
+                }
+            }
+            if(found == true){
+                ans = ans + 1;
+            }
+        }
+        return ans;
+    }
+}
