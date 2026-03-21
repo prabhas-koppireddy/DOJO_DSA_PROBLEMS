bf151faf35aabe6690f64f9a5e8e56143ecba736
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-03-21 17:14:57 +0530

    koko-eating-bananas.java

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -28,3 +28,29 @@ class Solution {
         return ans;
     }
 }
+
+// Brute-Force Code
+class Solution {
+
+    public static int timeTaken(int[] piles, int hours){
+        int n = piles.length;
+        int totalHours = 0;
+        for(int i = 0;i < n;i ++){
+            totalHours += Math.ceil((double)piles[i]/hours);
+        }
+        return totalHours;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length, max = Integer.MIN_VALUE;
+        for(int i = 0;i < n;i ++){
+            max = Math.max(max, piles[i]);
+        }
+        for(int i = 1;i <= max;i ++){
+            int reqTime = timeTaken(piles, i);
+            if(reqTime <= h){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
