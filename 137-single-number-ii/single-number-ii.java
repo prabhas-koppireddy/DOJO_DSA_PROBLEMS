2579a03106561ed0ad81833b771f93d212a1f05a
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-04-03 19:07:24 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (89.80%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,21 +1,27 @@
 class Solution {
     public int singleNumber(int[] nums) {
        int n = nums.length;
-       HashMap<Integer, Integer> map = new HashMap<>();
-       for(int i = 0;i < n;i ++){
-           if(map.containsKey(nums[i])){
-            int prev = map.get(nums[i]);
-            map.put(nums[i], prev + 1);
-           }
-           else{
-            map.put(nums[i], 1);
-           }
-       }
-       for(int key: map.keySet()){
-        if(map.get(key) == 1){
-            return key;
-        }
-       }
-       return -1;
+    //    HashMap<Integer, Integer> map = new HashMap<>();
+    //    for(int i = 0;i < n;i ++){
+    //        if(map.containsKey(nums[i])){
+    //         int prev = map.get(nums[i]);
+    //         map.put(nums[i], prev + 1);
+    //        }
+    //        else{
+    //         map.put(nums[i], 1);
+    //        }
+    //    }
+    //    for(int key: map.keySet()){
+    //     if(map.get(key) == 1){
+    //         return key;
+    //     }
+    //    }
+    //    return -1;
+    int first = 0, second = 0;
+    for(int i = 0;i < n;i ++){
+        first = (first ^ nums[i]) & (~second);
+        second = (second ^ nums[i]) & (~first);
+    }
+    return first;
     }
 }
