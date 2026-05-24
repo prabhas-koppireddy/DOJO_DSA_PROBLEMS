640e2a405aef62322138a0a8b43f89bcc43f3908
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-05-24 21:57:00 +0530

    Refactor containsDuplicate method implementation
    Refactor containsDuplicate method to use HashSet for improved performance.

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -20,3 +20,21 @@ class Solution {
         return false;
     }
 }
+
+// another logic
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        HashSet<Integer> ans = new HashSet<>();
+        for(int i : nums){
+            if(ans.contains(i)){
+                return true;
+            }
+        ans.add(i);
+        }
+        return false;
+    }
+}
+
+
+
