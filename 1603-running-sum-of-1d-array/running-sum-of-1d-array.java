64260cbe7426eb161251304495956d93d3fcf0df
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-05-24 22:18:46 +0530

    Optimize running sum calculation in Solution class

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.java
@@ -1,5 +1,17 @@
 class Solution {
     public int[] runningSum(int[] nums) {
+        int[] ans = new int[n];
+        ans[0] = nums[0];
+        for(int i = 1;i < n;i ++){
+            ans[i] = ans[i - 1] + nums[i];
+        }
+        return ans;
+    }
+}
+
+// optimal code
+class Solution {
+    public int[] runningSum(int[] nums) {
         int n = nums.length;
         for(int i = 1;i < n;i ++){
             nums[i] += nums[i - 1];
@@ -7,3 +19,4 @@ class Solution {
         return nums;
     }
 }
+
