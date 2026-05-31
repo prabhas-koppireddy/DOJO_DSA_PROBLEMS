6ea523bdba362bad47d6f10a5147443d721c72fc
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-05-31 20:08:53 +0530

    Optimize longestOnes method implementation
    Refactor longestOnes method for improved efficiency using a sliding window approach.

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,5 +1,27 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
+        int n = nums.length, maxLen = 0;
+        for(int i = 0;i < n;i ++){
+            int zerosCnt = 0;
+            for(int j = i;j < n;j ++){
+                if(nums[j] == 0){
+                    zerosCnt++;
+                }
+                if(zerosCnt <= k){
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        return maxLen;
+    }
+}
+
+// optimal
+class Solution {
+    public int longestOnes(int[] nums, int k) {
         int n = nums.length;
         int l = 0, r = 0, maxLen = 0, zerosCnt = 0;
         while(r < n){
