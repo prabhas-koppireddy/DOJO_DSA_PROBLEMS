6db85ce77a3044464b846dc71e867455c1458e36
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-08-13 09:58:49 +0530

    Time: 22 ms (95.12%) | Memory: 95.3 MB (32.99%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,17 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0;i < n;i ++){
-            int key = nums[i];
-            if(map.containsKey(key)){
-                int prev = map.get(key);
-                if(i - prev <= k){
-                    return true;
-                }
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            // Remove the element that is now outside the window
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
             }
-            map.put(key, i);
+            
+            // Try to add the current element. If it fails, a duplicate exists.
+            if (!set.add(nums[i])) {
+                return true;
+            }
         }
+        
         return false;
     }
 }
