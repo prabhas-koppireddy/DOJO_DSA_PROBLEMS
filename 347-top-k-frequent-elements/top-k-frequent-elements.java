9431389fa70485def93cd0dccc1a58dc474307bf
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-08-13 20:39:44 +0530

    Time: 15 ms (54.49%) | Memory: 47.9 MB (20.00%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,25 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n = nums.length;
-        int[] ans = new int[k];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0;i < n;i ++){
-            if(map.containsKey(nums[i])){
-                int prev = map.get(nums[i]);
-                map.put(nums[i], prev + 1);
-            }
-            else{
-                map.put(nums[i], 1);
-            }
+
+        // Step 1: Count frequency of every number
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int element : nums){
+            map.put(element, map.getOrDefault(element,0)+1);
         }
-        List<Integer> list = new ArrayList<>();
-        for(int key: map.keySet()){
-            list.add(key);
+
+        // Step 2: Convert HashMap into a List
+        ArrayList<Map.Entry<Integer,Integer>> arr =
+                new ArrayList<>(map.entrySet());
+
+        // Step 3: Sort by frequency in descending order
+        arr.sort((a,b)->b.getValue()-a.getValue());
+
+        // Step 4: Pick first K elements
+        int[] last = new int[k];
+
+        for(int i=0;i<k;i++){
+            last[i]=arr.get(i).getKey();
         }
-        Collections.sort(list, (a, b) -> map.get(b) - map.get(a));
-        for(int i = 0;i < k;i ++){
-            ans[i] = list.get(i);
-        }
-        return ans;
+
+        return last;
     }
 }
