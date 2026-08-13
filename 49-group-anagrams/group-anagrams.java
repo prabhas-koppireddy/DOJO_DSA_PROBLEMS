04a50988fb7c11f45c234a0dbc2cc54eaa34ea18
prabhas-koppireddy <prabhaskoppireddy@gmail.com>
2026-08-13 14:46:31 +0530

    Time: 7 ms (72.31%) | Memory: 49.5 MB (84.21%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,41 +1,19 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
-        for(String string: strs){
-            char[] temp = string.toCharArray();
-            Arrays.sort(temp);
-            String key = new String(temp);
-            if(!map.containsKey(key)){
-                map.put(key, new ArrayList<>());
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
             }
-            map.get(key).add(string);
+            
+            map.get(sortedWord).add(word);
         }
+        
         return new ArrayList<>(map.values());
     }
 }
-
-/**
-Create HashMap to store groups
-
-Start loop for each string in array
-
-Convert string to char array
-
-Sort the char array
-
-Convert sorted array to String key
-
-Check if key exists in HashMap
-
-If key not exists, create new ArrayList
-
-Add current string to list of that key
-
-Repeat for all strings
-
-Get all values from HashMap
-
-Convert values to List
-
-Return the result
- */
